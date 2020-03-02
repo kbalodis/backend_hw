a927ac47a2f2823fe09409d3314fa1315850c156
Krisjanis Balodis <KrisjanisB1@verifone.com>
2020-03-02 17:15:33 +0200

    python part done

---
+++ README.md
@@ -0,0 +1,11 @@
+# Backend HW
+
+Backend HW is a web application for adding, listing, retrieving, modifying, deleting and making statistcal analysis of specific reading objects. 
+
+## Prerequisites
+
+Flask and and flask-wtf libraries should be installed.
+
+```
+pip install flask flask-wtf scipy statsmodels numpy --user
+```
--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from records import Record
 import os
 import uuid
 from datetime import datetime
+import numpy as np
+from scipy.stats import shapiro
+from statsmodels.tsa.stattools import adfuller
 
 app = Flask(__name__)
 
@@ -17,7 +20,7 @@ RECORDS = []
 def home():
     return render_template('home.html', data=RECORDS)
 
-@app.route('/add', methods=('GET', 'POST'))
+@app.route('/add', methods=['GET', 'POST'])
 def add():
     form = RecordForm()
     if form.validate_on_submit():
@@ -28,20 +31,85 @@ def add():
         RECORDS.append(record)
         return redirect(url_for('home'))
     return render_template('add.html', form=form)
-    
-@app.route('/statistics')
-def statistics():
-    return render_template('statistics.html')
 
 @app.route('/delete/<string:id>', methods=['GET', 'POST'])
 def delete(id):
     index = 0
     for i, j in enumerate(RECORDS):
-        if j.id == id:
+        if str(j.id) == id:
             index = i
             break
     RECORDS.pop(index)
     return redirect(url_for('home'))
+
+@app.route('/retrieve/<string:id>', methods=['GET', 'POST'])
+def retrieve(id):
+    record = None
+    for item in RECORDS:
+        if str(item.id) == id:
+            record = item
+            break
+    return render_template('retrieve.html', id=id, data=record)
+
+@app.route('/edit/<string:id>', methods=['GET', 'POST'])
+def edit(id):
+    form = RecordForm()
+    if form.validate_on_submit():
+        for item in RECORDS:
+            if str(item.id) == id:
+                item.value = request.form['value']
+                item.timestamp = datetime.now()
+                break
+        return redirect(url_for('home'))
+    return render_template('edit.html', id=id, form=form)
+
+@app.route('/statistics', methods=['GET'])
+def statistics():
+    count = len(RECORDS)
+    records = []
+    for item in RECORDS:
+        records.append(item.value)
+    records = np.array(records).astype(np.float)
+    print(records)
+    mean = np.mean(records)
+    variance = np.var(records)
+    if len(records) > 2:
+        is_gaussian_shapiro = shapiro_test(records)
+        confidence_interval = confidence_interval_95(mean, np.std(records), count)
+    else:
+        is_gaussian_shapiro = False
+        confidence_interval = None
+    if len(records) > 29:
+        stationarity = ADF_test(records)
+    else:
+        stationarity = False
+    return render_template('statistics.html', count=count, mean=mean, variance=variance,\
+                           is_gaussian=is_gaussian_shapiro, confidence_interval=confidence_interval,\
+                           statoinarity=stationarity)
+
+def shapiro_test(array):
+    _, p = shapiro(array)
+    alpha = 0.05
+    if p > alpha:
+        return True
+    else:
+        return False
+
+def confidence_interval_95(mean, sd, count):
+    interval = []
+    se = sd / np.sqrt(count)
+    lower = mean - 1.96 * se
+    upper = mean + 1.96 * se
+    interval.append(lower)
+    interval.append(upper)
+    return interval
+
+def ADF_test(array):
+    adfTest = adfuller(array, autolag='AIC')
+    if adfTest[1] < 0.05:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     app.run(debug=True)
